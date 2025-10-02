d868e68db00a1bc6d825343b69f437ea3eb85cad
Julio Saráuz <jsarauz@29deoctubre.fin.ec>
2025-10-02 14:18:09 -0500

    fix main ts

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,13 @@ import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
-  const puerto = process.env.PORT ?? 3000;
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  Logger.warn('Aplicativo corriendo en el puerto: '+ puerto);
-  await app.listen(puerto);
+
+  Logger.warn(`Aplicativo corriendo en el puerto: ${port}`);
+
+  // 👇 Importante para Render
+  await app.listen(port, '0.0.0.0');
 }
 bootstrap();
